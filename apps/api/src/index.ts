2caef389a6a1a083460a0b986921dc518a25466a
wladimirgrf <wladimir.code@gmail.com>
2024-05-30 21:01:43 -0300

    Otimizando a tipagem das permissões

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -2,7 +2,7 @@ import { defineAbilityFor } from '@nivo/auth'
 
 const ability = defineAbilityFor({ role: 'MEMBER' })
 
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
+// const userCanInviteSomeoneElse = ability.can('invite', 'User')
 
 const userCanDeleteOtherUsers = ability.can('delete', 'User')
 
