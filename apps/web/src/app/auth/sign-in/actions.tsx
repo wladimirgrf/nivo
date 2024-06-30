7552cc12e07d92d715b7bbf65c5139757a1d5227
wladimirgrf <wladimir.code@gmail.com>
2024-06-30 13:25:20 -0300

    Usando o hook useActionState

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -2,7 +2,10 @@
 
 import { signInWithPassword } from '@/http/sign-in-with-password'
 
-export async function signInWithEmailAndPassword(data: FormData) {
+export async function signInWithEmailAndPassword(
+  previusState: unknown,
+  data: FormData,
+) {
   const { email, password } = Object.fromEntries(data)
 
   const result = await signInWithPassword({
