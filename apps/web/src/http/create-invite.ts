8e4c920dedb1afa79b61e7fda9132176dae17422
wladimirgrf <wladimir.code@gmail.com>
2024-08-11 11:52:52 -0300

    Convidar novo usuário

---
+++ apps/web/src/http/create-invite.ts
@@ -0,0 +1,21 @@
+import { Role } from '@nivo/auth'
+
+import { api } from './api-client'
+
+interface CreateInviteRequest {
+  org: string
+  email: string
+  role: Role
+}
+
+type CreateInviteResponse = void
+
+export async function createInvite({
+  org,
+  email,
+  role,
+}: CreateInviteRequest): Promise<CreateInviteResponse> {
+  await api.post(`organizations/${org}/invites`, {
+    json: { email, role },
+  })
+}
