c0dfeb3c50eb93776b985e32c4082f42304cd23a
wladimirgrf <wladimir.code@gmail.com>
2024-08-12 18:59:21 -0300

    Listagem de projetos

--- apps/web/src/app/(app)/org/[slug]/(projects)/project-list.tsx
+++ apps/web/src/app/(app)/org/[slug]/(projects)/project-list.tsx
@@ -1,5 +1,8 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import { ArrowRight } from 'lucide-react'
 
+import { getCurrentOrg } from '@/auth/auth'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import {
@@ -9,37 +12,49 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
+import { fetchProjects } from '@/http/fetch-projects'
 
-export function ProjectList() {
+dayjs.extend(relativeTime)
+
+export async function ProjectList() {
+  const currentOrg = getCurrentOrg()
+  const { projects } = await fetchProjects(currentOrg!)
+
   return (
     <div className="grid grid-cols-3 gap-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Project 01</CardTitle>
-          <CardDescription className="line-clamp-2 leading-relaxed">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas harum
-            dicta, culpa cupiditate debitis reiciendis blanditiis nesciunt
-            recusandae tenetur commodi eius animi unde deserunt, voluptatum
-            soluta quaerat labore in autem!
-          </CardDescription>
-        </CardHeader>
-        <CardFooter className="flex items-center gap-1.5">
-          <Avatar className="size-4">
-            <AvatarImage src="https://github.com/wladimirgrf.png" />
-            <AvatarFallback />
-          </Avatar>
+      {projects.map((project) => {
+        return (
+          <Card key={project.id} className="flex flex-col justify-between">
+            <CardHeader>
+              <CardTitle className="text-xl font-medium">
+                {project.name}
+              </CardTitle>
+              <CardDescription className="line-clamp-2 leading-relaxed">
+                {project.description}
+              </CardDescription>
+            </CardHeader>
+            <CardFooter className="flex items-center gap-1.5">
+              <Avatar className="size-4">
+                {project.owner.avatarUrl && (
+                  <AvatarImage src={project.owner.avatarUrl} />
+                )}
+                <AvatarFallback />
+              </Avatar>
 
-          <span className="text-xs text-muted-foreground">
-            Created by{' '}
-            <span className="font-medium text-foreground">Wlad Wasalsk</span> a
-            day ago
-          </span>
+              <span className="truncate text-xs text-muted-foreground">
+                <span className="font-medium text-foreground">
+                  {project.owner.name}
+                </span>{' '}
+                {dayjs(project.createdAt).fromNow()}
+              </span>
 
-          <Button size="xs" variant="outline" className="ml-auto">
-            View <ArrowRight className="ml-2 size-3" />
-          </Button>
-        </CardFooter>
-      </Card>
+              <Button size="xs" variant="outline" className="ml-auto">
+                View <ArrowRight className="ml-2 size-3" />
+              </Button>
+            </CardFooter>
+          </Card>
+        )
+      })}
     </div>
   )
 }
