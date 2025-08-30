9c2a04cdf05abccb304cb41e344397a0b0299250
wladimirgrf <20077278+wladimirgrf@users.noreply.github.com>
2025-08-30 18:04:45 -0300

    feat: terraform set up

---
+++ iac/eks.tf
@@ -0,0 +1,35 @@
+resource "aws_eks_cluster" "main" {
+  name     = "nivo-cluster"
+  role_arn = aws_iam_role.eks_cluster.arn
+  version  = "1.28"
+
+  vpc_config {
+    subnet_ids              = aws_subnet.public[*].id
+    endpoint_public_access  = true
+    endpoint_private_access = false
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+}
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "nivo-node-group"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = aws_subnet.public[*].id
+  instance_types  = ["t3.small"]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_container_registry_read_only
+  ]
+}
