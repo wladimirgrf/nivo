1feb219e90a8d1f829acf4a0958556e2b9aea086
wladimirgrf <20077278+wladimirgrf@users.noreply.github.com>
2025-08-31 16:04:00 -0300

    bugfix: eks deploy pt3

--- iac/eks.tf
+++ iac/eks.tf
@@ -3,6 +3,11 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.eks_cluster.arn
   version  = "1.33"
 
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   vpc_config {
     subnet_ids              = aws_subnet.public[*].id
     endpoint_public_access  = true
