b2fccb48072ea824eaf80daa1e6fe0f181eacda0
wladimirgrf <20077278+wladimirgrf@users.noreply.github.com>
2025-09-13 14:12:08 -0300

    bugfix: eks deploy pt9

--- iac/eks.tf
+++ iac/eks.tf
@@ -39,4 +39,20 @@ resource "aws_eks_node_group" "main" {
   ]
 }
 
+resource "aws_eks_access_entry" "github_actions" {
+  cluster_name      = aws_eks_cluster.main.name
+  principal_arn     = var.github_actions_role_arn
+  type             = "STANDARD"
+}
 
+resource "aws_eks_access_policy_association" "github_actions_admin" {
+  cluster_name  = aws_eks_cluster.main.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = var.github_actions_role_arn
+
+  access_scope {
+    type = "cluster"
+  }
+
+  depends_on = [aws_eks_access_entry.github_actions]
+}
