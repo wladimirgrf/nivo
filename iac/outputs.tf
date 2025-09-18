cb7aeeffe2424f3e7a91dfb61059b97904dfe151
wladimirgrf <20077278+wladimirgrf@users.noreply.github.com>
2025-09-18 18:12:22 -0300

    general improvements

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,14 +1,9 @@
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = aws_eks_cluster.main.endpoint
-}
-
-output "cluster_name" {
+output "eks_cluster_name" {
   description = "Name of the EKS cluster"
   value       = aws_eks_cluster.main.name
 }
 
-output "ecr_repository_url" {
-  description = "URL of the ECR repository"
-  value       = aws_ecr_repository.api.repository_url
+output "ecr_repo_name" {
+  description = "Name of the ECR repository"
+  value       = aws_ecr_repository.api.name
 }
